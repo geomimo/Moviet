2c399c5a2bcbf7035ab1028e8ed68f76d4b0c6d4
geomimo <mimoglou22@gmail.com>
2020-05-15 21:43:48 +0300

    Initial commit

---
+++ Moviet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moviet.Data;
+
+[assembly: HostingStartup(typeof(Moviet.Areas.Identity.IdentityHostingStartup))]
+namespace Moviet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

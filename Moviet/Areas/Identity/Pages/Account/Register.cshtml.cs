34f26b45c30e2d89efc9e7edb67b2f83b6074d7e
geomimo <mimoglou22@gmail.com>
2020-05-20 13:15:32 +0300

    Set role on registration

--- Moviet/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Moviet/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -82,6 +82,7 @@ namespace Moviet.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, Roles.Rater).Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     /*

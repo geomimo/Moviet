73cacddfb34f6bdaed59c955fffe7615b4df2882
geomimo <mimoglou22@gmail.com>
2020-05-20 19:48:07 +0300

    Added fa brand, changed navbar

---
+++ Moviet/Services/RaterService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Moviet.Services
+{
+    public class RaterService
+    {
+    }
+}

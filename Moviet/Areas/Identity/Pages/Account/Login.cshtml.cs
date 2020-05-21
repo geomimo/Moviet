8e4218ef2d1bd8d3228f429b77a6fcd584ffbb1d
geomimo <mimoglou22@gmail.com>
2020-05-21 18:15:42 +0300

    Added nav links account, your ratings, your registrations

--- Moviet/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Moviet/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -43,7 +43,6 @@ namespace Moviet.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
             public string Username { get; set; }
 
             [Required]

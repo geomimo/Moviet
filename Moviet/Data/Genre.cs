66cbb1e0a4ef36c6720e9d002abc4982c2345edc
John <giannos-okkas@hotmail.com>
2020-05-22 14:34:23 +0300

    Added movie propertis and created a list of Genres

---
+++ Moviet/Data/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moviet.Data
+{
+    public enum Genre
+    {
+        Action,
+        Thriller,
+        Horror,
+        Comedy,
+        Drama,
+        SciFi,
+        Romantic
+
+    }
+}
---
+++ Moviet/Data/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moviet.Data
+{
+    public class Movie
+    {
+        [Key]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public float Rating { get; set; }
+        public string PosterPath { get; set; }
+        public string YoutubeId { get; set; }
+        public List<Genre> Genres { get; set; }
+
+    }
+}

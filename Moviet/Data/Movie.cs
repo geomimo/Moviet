41946bc2b22470e4f097ad14fee9e9c2aa520d69
geomimo <mimoglou22@gmail.com>
2020-05-23 12:29:24 +0300

    Added movieid

--- Moviet/Data/Movie.cs
+++ Moviet/Data/Movie.cs
@@ -9,6 +9,7 @@ namespace Moviet.Data
     public class Movie
     {
         [Key]
+        public int MovieId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public float Rating { get; set; }

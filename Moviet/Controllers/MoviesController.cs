09c6fbe42b9e028b520b141fa2529f60bb9d6f7b
geomimo <mimoglou22@gmail.com>
2020-05-26 15:04:35 +0300

    Added movie details

--- Moviet/Controllers/MoviesController.cs
+++ Moviet/Controllers/MoviesController.cs
@@ -21,10 +21,18 @@ namespace Moviet.Controllers
             _postrepo = postrepo;
             _mapper = mapper;
         }   
+
         public IActionResult Index()
         {
             List<Post> posts = _postrepo.FindAll();
             List<PostVM> model = _mapper.Map<List<PostVM>>(posts);
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Post post = _postrepo.FindById(id);
+            PostVM model = _mapper.Map<PostVM>(post);
             return View(model);
         }
     }

949aaaa08b9617785a1f51c587ca4c254b27e7e2
geomimo <mimoglou22@gmail.com>
2020-05-31 17:37:33 +0300

    Changed movie index overloading to various actions, fixed contentmanagerpostlinkpartial passing data.

--- Moviet/Controllers/MoviesController.cs
+++ Moviet/Controllers/MoviesController.cs
@@ -14,11 +14,13 @@ namespace Moviet.Controllers
     public class MoviesController : Controller
     {
         private readonly IPostRepository _postrepo;
+        private readonly IGenreRepository _genrerepo;
         private readonly IMapper _mapper;
 
-        public MoviesController(IPostRepository postrepo, IMapper mapper)
+        public MoviesController(IPostRepository postrepo, IMapper mapper, IGenreRepository genrerepo)
         {
             _postrepo = postrepo;
+            _genrerepo = genrerepo;
             _mapper = mapper;
         }
 
@@ -26,33 +28,44 @@ namespace Moviet.Controllers
         {
             List<Post> posts = _postrepo.FindAll();
             List<PostVM> model = _mapper.Map<List<PostVM>>(posts);
-            return View(model);
+
+            List<Genre> genres = _genrerepo.FindAll();
+            List<GenreVM> genresVM = _mapper.Map<List<GenreVM>>(genres); 
+
+
+            ViewData["Title"] = "All Movies";
+            return View("Index", model);
         }
 
-        [HttpGet("{mode:regex(topRated|newReleases)}")]
-        public IActionResult Index(string mode)
+        public IActionResult TopRated()
         {
             List<Post> posts = _postrepo.FindAll();
-            List<PostVM> model = _mapper.Map<List<PostVM>>(posts);
-            if (mode.Equals("topRated"))
-            {
-                model = model.OrderByDescending(p => p.Movie.Rating).ToList();
-            }
-            else if (mode.Equals("newReleases"))
-            {
-                model = model.OrderByDescending(p => p.DateCreated).ToList();
-            }
-            return View(model);
+            List<PostVM> model = _mapper.Map<List<PostVM>>(posts);         
+            model = model.OrderByDescending(p => p.Movie.Rating).ToList();
+
+            ViewData["Title"] = "Top Rated";
+            return View("Index", model);
         }
 
+        public IActionResult NewReleases()
+        {
+            List<Post> posts = _postrepo.FindAll();
+            List<PostVM> model = _mapper.Map<List<PostVM>>(posts); 
+            model = model.OrderByDescending(p => p.DateCreated).ToList();
 
-        // TODO
-        [HttpGet("Index/ByGenre/{id:int}")]
-        public IActionResult Index(int id)
+            ViewData["Title"] = "New Releases";
+            return View("Index", model);
+        }
+
+        public IActionResult ByGenre(int id)
         {
             List<Post> posts = _postrepo.FindAllByGenreId(id);
             List<PostVM> model = _mapper.Map<List<PostVM>>(posts);
-            return View(model);
+
+            string genre = _genrerepo.FindById(id).Name;
+
+            ViewData["Title"] = genre + " Movies";
+            return View("Index", model);
         }
 
 

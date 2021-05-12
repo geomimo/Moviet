43673662b5651cd37fe9cac12e641cfcde921eff
George Mimoglou <mimoglou22@gmail.com>
2021-05-12 19:07:17 +0300

    Added pagination and search for admin

--- Moviet/Controllers/ListController.cs
+++ Moviet/Controllers/ListController.cs
@@ -9,7 +9,7 @@ using Moviet.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+using X.PagedList;
 
 namespace Moviet.Controllers
 {
@@ -21,73 +21,6 @@ namespace Moviet.Controllers
         private readonly IBanService _banService;
         private readonly IPostRepository _postrepo;
 
-        public string certHeader = "";
-        public string errorString = "";
-        private X509Certificate2 certificate = null;
-        public string certThumbprint = "";
-        public string certSubject = "";
-        public string certIssuer = "";
-        public string certSignatureAlg = "";
-        public string certIssueDate = "";
-        public string certExpiryDate = "";
-        public bool isValidCert = false;
-
-
-        private bool IsValidClientCertificate()
-        {
-            // In this example we will only accept the certificate as a valid certificate if all the conditions below are met:
-            // 1. The certificate is not expired and is active for the current time on server.
-            // 2. The subject name of the certificate has the common name nildevecc
-            // 3. The issuer name of the certificate has the common name nildevecc and organization name Microsoft Corp
-            // 4. The thumbprint of the certificate is 30757A2E831977D8BD9C8496E4C99AB26CB9622B
-            //
-            // This example does NOT test that this certificate is chained to a Trusted Root Authority (or revoked) on the server 
-            // and it allows for self signed certificates
-            //
-
-            if (certificate == null || !String.IsNullOrEmpty(errorString)) return false;
-
-            // 1. Check time validity of certificate
-            if (DateTime.Compare(DateTime.Now, certificate.NotBefore) < 0 || DateTime.Compare(DateTime.Now, certificate.NotAfter) > 0) return false;
-
-            // 2. Check subject name of certificate
-            bool foundSubject = false;
-            string[] certSubjectData = certificate.Subject.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string s in certSubjectData)
-            {
-                if (String.Compare(s.Trim(), "CN=nildevecc") == 0)
-                {
-                    foundSubject = true;
-                    break;
-                }
-            }
-            if (!foundSubject) return false;
-
-            // 3. Check issuer name of certificate
-            bool foundIssuerCN = false, foundIssuerO = false;
-            string[] certIssuerData = certificate.Issuer.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string s in certIssuerData)
-            {
-                if (String.Compare(s.Trim(), "CN=nildevecc") == 0)
-                {
-                    foundIssuerCN = true;
-                    if (foundIssuerO) break;
-                }
-
-                if (String.Compare(s.Trim(), "O=Microsoft Corp") == 0)
-                {
-                    foundIssuerO = true;
-                    if (foundIssuerCN) break;
-                }
-            }
-
-            if (!foundIssuerCN || !foundIssuerO) return false;
-
-            // 4. Check thumprint of certificate
-            if (String.Compare(certificate.Thumbprint.Trim().ToUpper(), "30757A2E831977D8BD9C8496E4C99AB26CB9622B") != 0) return false;
-
-            return true;
-        }
 
         public ListController(UserManager<ApplicationUser> userManager,
                               IMapper mapper,
@@ -100,14 +33,24 @@ namespace Moviet.Controllers
             _postrepo = postrepo;
         }
 
-        public IActionResult AllUsers()
+        public IActionResult AllUsers(int? page, string searchString = null)
         {
+
+
             var raters = _userManager.GetUsersInRoleAsync(Roles.Rater).Result;
             var contentManager = _userManager.GetUsersInRoleAsync(Roles.ContentManager).Result;
             var allUsers = new List<ApplicationUser>();
             allUsers.AddRange(raters);
             allUsers.AddRange(contentManager);
             allUsers.Sort((p, q) => p.UserName.CompareTo(q.UserName));
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+
+                ViewData["CurrentFilter"] = searchString;
+                allUsers = allUsers.Where(s => s.UserName.ToLower().Contains(searchString.ToLower())
+                                       || s.Email.ToLower().Contains(searchString.ToLower())).ToList();
+            }
 
             var model = _mapper.Map<List<IdentityUserVM>>(allUsers);
 
@@ -118,16 +61,28 @@ namespace Moviet.Controllers
                 m.Role = role;
             }
 
-            return View(model);
+            int pageSize = 12;
+            int pageNumber = (page ?? 1);
+
+            return View(model.ToPagedList(pageNumber, pageSize));
         }
 
-        public IActionResult AllPosts()
-        {
+        public IActionResult AllPosts(int? page, string searchString = null)
+        { 
             var posts = _postrepo.FindAll();
             posts.Sort((p, q) => p.DateCreated.CompareTo(q.DateCreated));
+
+            if (!String.IsNullOrEmpty(searchString))
+            {        
+                ViewData["CurrentFilter"] = searchString;  
+                posts = posts.Where(s => s.Movie.Title.ToLower().Contains(searchString.ToLower())).ToList();
+            }      
+
             var model = _mapper.Map<List<PostVM>>(posts);
 
-            return View(model);
+            int pageSize = 12;
+            int pageNumber = (page ?? 1);
+            return View(model.ToPagedList(pageNumber, pageSize));
         }
 
         public IActionResult BanUser(string id)
